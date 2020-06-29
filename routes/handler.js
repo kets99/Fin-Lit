f2b8296c11f9054fdd8b35404be803a93ee4bcd4
Ketki Davda <ketki.davda@somaiya.edu>
2020-06-29 01:00:27 -0700

    Update handler.js

--- routes/handler.js
+++ routes/handler.js
@@ -142,35 +142,6 @@ router.get('/facebook/callback', function (req, res) {
            }
        });  
  
-       var url = 'https://graph.facebook.com/me/feed';
-       var params = {
-       access_token: accessToken,
-       message: "I reached the Intermediate level on Fin-Lit.",
-       link: req.body.url
-      
-       };
-       request.post({url: url, qs: params}, function(err, resp, body) {
-           if (err) {
-           console.error(err)
-           return;
-           }
-           body = JSON.parse(body);
-           if (body.error) {
-               var error = body.error.message;
-               console.error("Error returned from facebook: "+ body.error.message);
-               if (body.error.code == 341) {
-                   error = "You have reached the post limit for facebook. Please wait for 24 hours before posting again to facebook."
-                   console.error(error);
-               }
-               res.send(error);
-           }
-           console.log(body);
-           var return_ids = body.id.split('_');
-           var user_id = return_ids[0];
-           var post_id = return_ids[1];
-           var post_url = "https://www.facebook.com/"+user_id+"/posts/"+post_id;
-           res.send(post_url);
-       });
       }
     );
    }
